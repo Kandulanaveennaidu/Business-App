fa9cfcfe89969a19957d2d6f60246550d185ec2d
Kandula Naveen <kandulanaveennaidu017@gmail.com>
2023-12-26 10:53:52 +0530

    first

--- src/components/Services.js
+++ src/components/Services.js
@@ -28,19 +28,19 @@ const Services = () => {
       <div className="flex gap-10 items-center flex-col-reverse lg:flex-row">
         <div className="relative w-full flex-1">
           <Image
-            src={"/portifilo.png"}
+            src={"/videobanner.png"}
             width={500}
             height={500}
             alt="video banner"
             className="object-cover w-full"
           />
           {/* <h1 className="object-cover w-full" >portfolio</h1> */}
-          <button className="w-32 h-32 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          {/* <button className="w-32 h-32 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <Link href={"https://professional-portfolio-6i2o.vercel.app/"} target="_blank">
               <span className="playButton duration-700 w-full h-full bg-rose-500 rounded-full block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></span>
               <PlayArrowIcon className="w-16 h-16 md:w-24 md:h-24 text-white bg-[#ffffffb8] rounded-full" />
             </Link>
-          </button>
+          </button> */}
         </div>
         <div className="flex gap-5 md:gap-10 flex-col flex-1">
           <div className="flex flex-col gap-4">
